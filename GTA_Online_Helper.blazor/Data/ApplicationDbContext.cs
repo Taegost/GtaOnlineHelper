67d810fb863fc6b4ed38bfbc1fad3f5c2b4b337e
Mike Wheway <taegost@gmail.com>
2022-08-26 18:14:45 -0400

    Added Blazor front end

---
+++ GTA_Online_Helper.blazor/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GTA_Online_Helper.blazor.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
